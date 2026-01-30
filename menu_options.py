dc9d820181a2c07565618af2cf79e64d34ce424e
Mikel Gomez-Ortega <mikel.gomez-ortega@resonate.tech>
2026-01-30 14:52:01 +0000

    Fix program still running after pressing x button on TopLevel screen

--- menu_options.py
+++ menu_options.py
@@ -7,6 +7,7 @@ class MenuOptions:
         self.window.geometry("500x800")
         self.window.resizable(False, False)
         self.window.withdraw()
+        self.window.protocol("WM_DELETE_WINDOW", self.on_close) # AI helped me implement this initially, as I had bugs related to this specific issue.
 
         self.master = master
         self.calc = calc
@@ -25,3 +26,13 @@ class MenuOptions:
         self.calc.closeCalcMenu()
         self.window.title("Tables and Statistics")
         self.window.wm_deiconify()
+
+    def on_close(self): # Created with some help from AI, as mentioned previously, this gave me a bug that I struggled to fix.
+        try:
+            self.calc.calcWindow.destroy()
+        except Exception:
+            pass
+        try:
+            self.window.destroy()
+        except Exception:
+            pass
