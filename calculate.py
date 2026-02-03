2379e95ac22a25ee5b6888dc4e1c06522fa45d05
Mikel Gomez-Ortega <mikel.gomez-ortega@resonate.tech>
2026-02-03 15:33:55 +0000

    Fix calculator display label not packing where intended

--- calculate.py
+++ calculate.py
@@ -10,13 +10,18 @@ class Calculator():
                                    borderwidth=0, highlightthickness=0)
         self.button_frame = tk.Frame(self.master, width=500, height=450, bg="#999999",
                                      borderwidth=0, highlightthickness=0)
-        
-        self.text_frame.pack_propagate(False)
-        self.button_frame.grid_propagate(False)
 
         self.calc_text_label = tk.Label(self.text_frame, text="Calculator Display",
                                        font=("Georgia", 24), bg="white")
         
-        self.text_frame.grid(row=1, column=0, columnspan=4)
-        self.button_frame.grid(row=2, column=0, columnspan=4)
-        self.calc_text_label.pack(anchor="se", padx=10, pady=10)
+        self.text_frame.grid_propagate(False)
+        self.button_frame.grid_propagate(False)
+
+        self.master.grid_rowconfigure(1, minsize=280)
+        self.master.grid_rowconfigure(2, minsize=450)
+        self.master.grid_columnconfigure(0, minsize=500)
+
+        self.text_frame.grid(row=1, column=0, sticky="nsew")
+        self.button_frame.grid(row=2, column=0, sticky="nsew")
+
+        self.calc_text_label.pack(side="bottom", anchor="w", padx=20, pady=20)
