24cd5263e0a893b88ada378cf29b4aad717bf0c0
Mikel Gomez-Ortega <mikel.gomez-ortega@resonate.tech>
2026-02-03 14:57:00 +0000

    Create frame to place number buttons onto

--- calculate.py
+++ calculate.py
@@ -6,4 +6,10 @@ class Calculator():
 
     def show(self):
         self.master.title("Calculator Screen")
+        self.text_frame = tk.Frame(self.master, width=500, height=280, bg="white",
+                                   borderwidth=0, highlightthickness=0)
+        self.button_frame = tk.Frame(self.master, width=500, height=450, bg="#999999",
+                                     borderwidth=0, highlightthickness=0)
         
+        self.text_frame.grid(row=1, column=0, columnspan=4)
+        self.button_frame.grid(row=2, column=0, columnspan=4)
