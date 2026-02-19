105410540d066a39a54c0365099b12d476b5c261
Mikel Gomez-Ortega <mikel.gomez-ortega@resonate.tech>
2026-02-19 11:28:56 +0000

    Create simple landing page widgets and layout

--- table.py
+++ table.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import os
 
 class Tables():
     def __init__(self, master):
@@ -9,3 +10,17 @@ class Tables():
 
         # Import a CSV file which will be read from to create table
         # Once table created, user can find mean, variance, standard deviation etc...
+        self.entry_frame = tk.Frame(self.master, width=500, height=730)
+        self.entry_frame.pack_propagate(False)
+        self.entry_frame.grid()
+
+        self.title_label = tk.Label(self.entry_frame, text="Input file name:", font=("Georgia", 24, "bold"))
+        self.title_label.pack(anchor='n', pady=50)
+
+        self.file_entry = tk.Entry(self.entry_frame, width=40)
+        self.file_entry.pack(anchor='n')
+
+        self.entry_button = tk.Button(self.entry_frame, width=10, height=1, bg='white', 
+                                      borderwidth=0, text='Open file', font=("Georgia", 14))
+        self.entry_button.pack(anchor='n', pady=20)
+
