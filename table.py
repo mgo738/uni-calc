aa1f947b60e31f957cc6914cf7f6906f1e74dba8
Mikel Gomez-Ortega <mikel.gomez-ortega@resonate.tech>
2026-02-19 11:36:25 +0000

    Add error handling to ensure file exists and only allows .csv files

--- table.py
+++ table.py
@@ -4,6 +4,7 @@ import os
 class Tables():
     def __init__(self, master):
         self.master = master
+        self.user_file = ""
 
     def show(self):
         self.master.title("Table and Statistics")
@@ -18,9 +19,24 @@ class Tables():
         self.title_label.pack(anchor='n', pady=50)
 
         self.file_entry = tk.Entry(self.entry_frame, width=40)
+        self.file_entry.bind("<Return>", lambda e: self.show_table())
         self.file_entry.pack(anchor='n')
 
         self.entry_button = tk.Button(self.entry_frame, width=10, height=1, bg='white', 
-                                      borderwidth=0, text='Open file', font=("Georgia", 14))
+                                      borderwidth=0, text='Open file', font=("Georgia", 14),
+                                      command=self.show_table)
         self.entry_button.pack(anchor='n', pady=20)
 
+        self.error_label = tk.Label(self.entry_frame, font=("Georgia", 20, "bold"), fg="#B10A0A", text="")
+        self.error_label.pack(anchor='n', pady=10)
+    
+    def show_table(self):
+        self.user_file = self.file_entry.get().strip()
+
+        if not os.path.isfile(self.user_file):
+            self.error_label.config(text="Error: File does not exist.")
+            return None
+        
+        if not self.user_file.endswith(".csv"):
+            self.error_label.config(text="Error: File type must be .csv only")
+            return None
