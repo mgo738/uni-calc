f115aa7713c3290048f656b64bb1e20b4a38f572
Mikel Gomez-Ortega <mikel.gomez-ortega@resonate.tech>
2026-02-19 13:06:24 +0000

    Add different button to use file typed by user and one to open a selected file

--- table.py
+++ table.py
@@ -1,10 +1,12 @@
 import tkinter as tk
+from tkinter import ttk, filedialog
 import os
 
 class Tables():
     def __init__(self, master):
         self.master = master
         self.user_file = ""
+
 
     def show(self):
         self.master.title("Table and Statistics")
@@ -19,19 +21,31 @@ class Tables():
         self.title_label.pack(anchor='n', pady=50)
 
         self.file_entry = tk.Entry(self.entry_frame, width=40)
-        self.file_entry.bind("<Return>", lambda e: self.show_table())
+        self.file_entry.bind("<Return>", lambda e: self.use_file("use"))
         self.file_entry.pack(anchor='n')
 
-        self.entry_button = tk.Button(self.entry_frame, width=10, height=1, bg='white', 
+        self.use_button = tk.Button(self.entry_frame, width=10, height=1, bg='white', 
+                                      borderwidth=0, text='Use file', font=("Georgia", 14),
+                                      command=lambda: self.use_file("use"))
+        self.use_button.pack(anchor='n', pady=20)
+
+        self.open_button = tk.Button(self.entry_frame, width=10, height=1, bg='white', 
                                       borderwidth=0, text='Open file', font=("Georgia", 14),
-                                      command=self.show_table)
-        self.entry_button.pack(anchor='n', pady=20)
+                                      command=lambda: self.use_file("open"))
+        self.open_button.pack(anchor='n', pady=20)
 
         self.error_label = tk.Label(self.entry_frame, font=("Georgia", 18, "bold"), fg="#B10A0A", text="")
         self.error_label.pack(anchor='n', pady=10)
     
-    def show_table(self):
-        self.user_file = self.file_entry.get().strip()
+
+    def use_file(self, use):
+        if use == "use":
+            self.user_file = self.file_entry.get().strip()
+        elif use == "open":
+            self.user_file = filedialog.askopenfilename(
+                title="Select CSV File",
+                filetypes=[("CSV Files", "*.csv"), ("All Files", "*.*")]
+            )
 
         if not os.path.isfile(self.user_file):
             self.error_label.config(text="Error: File does not exist.")
@@ -40,7 +54,11 @@ class Tables():
         if not self.user_file.endswith(".csv"):
             self.error_label.config(text="Error: File type must be .csv only")
             return None
-        
+
+        self.show_table()
+    
+
+    def show_table(self):
         self.entry_frame.grid_forget()
 
         self.table_frame = tk.Frame(self.master, width=500, height=730, bg='red')
